e1d76e8a451126501722e454e3144aead807664f
HyunjoonCho <jhj09016@gmail.com>
2024-01-21 22:05:47 +0900

    feat: pass single token tests

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,18 +1,32 @@
 #include <stdlib.h>
+#include <string.h>
 #include "tokenizer.h"
 
 struct tokenizer {
-    char input[100];
+    char *input;
 };
 
 tokenizer_handle tokenizer_init(char *input) {
     tokenizer_handle new_tokenizer = malloc(sizeof(tokenizer));
-    return 0;
+    char *new_input = malloc(sizeof(*input));
+    strcpy(new_input, input);
+    new_tokenizer->input = new_input;
+    return new_tokenizer;
 }
 
 token_t get_next_token(tokenizer_handle tokenizer) {
     token_t next_token;
-    next_token.type = UNKNOWN;
-    next_token.value = "";
+    if (strchr(tokenizer->input, '+') != NULL) {
+        next_token.type = ADD_OPERATOR;
+    } else if (strchr(tokenizer->input, '-') != NULL) {
+        next_token.type = SUBTRACT_OPERATOR;
+    } else if (strchr(tokenizer->input, '.') != NULL) {
+        next_token.type = FLOAT;
+    } else {
+        next_token.type = INTEGER;
+    }
+    char *new_value = malloc(sizeof(*tokenizer->input));
+    strcpy(new_value,tokenizer->input);
+    next_token.value = new_value;
     return next_token;
 }
