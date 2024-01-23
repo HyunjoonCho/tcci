3a2ca62fbe1ba5c0c7cbfe48739cb4ba510f4fca
HyunjoonCho <jhj09016@gmail.com>
2024-01-24 00:06:03 +0900

    refactor: change parse_expression signature to return pointer

--- tests/test_parser.cpp
+++ tests/test_parser.cpp
@@ -13,15 +13,15 @@ TEST(ParserTest, ParseSimpleExpression) {
     };
 
     // Parse the array of tokens
-    expr_t expr = parse_expression(tokens, sizeof(tokens) / sizeof(tokens[0]));
+    expr_t *expr = parse_expression(tokens, sizeof(tokens) / sizeof(tokens[0]));
 
     // Assert the parsed expression
-    EXPECT_EQ(expr.type, BINARY_OP);
-    EXPECT_EQ(expr.op, ADD);
-    EXPECT_EQ(expr.left_operand->type, LITERAL);
-    EXPECT_STREQ(expr.left_operand->value, "2");
-    EXPECT_EQ(expr.right_operand->type, LITERAL);
-    EXPECT_STREQ(expr.right_operand->value, "3");
+    EXPECT_EQ(expr->type, BINARY_OP);
+    EXPECT_EQ(expr->op, ADD);
+    EXPECT_EQ(expr->left_operand->type, LITERAL);
+    EXPECT_STREQ(expr->left_operand->value, "2");
+    EXPECT_EQ(expr->right_operand->type, LITERAL);
+    EXPECT_STREQ(expr->right_operand->value, "3");
 }
 
 TEST(ParserTest, ParseExpressionWithPriority) {
@@ -35,19 +35,19 @@ TEST(ParserTest, ParseExpressionWithPriority) {
     };
 
     // Parse the array of tokens
-    expr_t expr = parse_expression(tokens, sizeof(tokens) / sizeof(tokens[0]));
+    expr_t *expr = parse_expression(tokens, sizeof(tokens) / sizeof(tokens[0]));
 
     // Assert the parsed expression
-    EXPECT_EQ(expr.type, BINARY_OP);
-    EXPECT_EQ(expr.op, SUBTRACT);
-    EXPECT_EQ(expr.left_operand->type, LITERAL);
-    EXPECT_STREQ(expr.left_operand->value, "2");
+    EXPECT_EQ(expr->type, BINARY_OP);
+    EXPECT_EQ(expr->op, SUBTRACT);
+    EXPECT_EQ(expr->left_operand->type, LITERAL);
+    EXPECT_STREQ(expr->left_operand->value, "2");
 
     // Right operand should be another binary op
-    EXPECT_EQ(expr.right_operand->type, BINARY_OP);
-    EXPECT_EQ(expr.right_operand->op, MULTIPLY);
-    EXPECT_EQ(expr.right_operand->left_operand->type, LITERAL);
-    EXPECT_STREQ(expr.right_operand->left_operand->value, "3");
-    EXPECT_EQ(expr.right_operand->right_operand->type, LITERAL);
-    EXPECT_STREQ(expr.right_operand->right_operand->value, "4");
+    EXPECT_EQ(expr->right_operand->type, BINARY_OP);
+    EXPECT_EQ(expr->right_operand->op, MULTIPLY);
+    EXPECT_EQ(expr->right_operand->left_operand->type, LITERAL);
+    EXPECT_STREQ(expr->right_operand->left_operand->value, "3");
+    EXPECT_EQ(expr->right_operand->right_operand->type, LITERAL);
+    EXPECT_STREQ(expr->right_operand->right_operand->value, "4");
 }
