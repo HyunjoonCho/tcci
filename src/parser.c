ce0463b2d973fb89a4b9e4a19dfef0c3ab3a7bd1
HyunjoonCho <jhj09016@gmail.com>
2024-01-24 00:08:04 +0900

    refactor: separate token to expr function on parser

--- src/parser.c
+++ src/parser.c
@@ -2,25 +2,30 @@
 #include <string.h>
 #include "commons.h"
 
-expr_t parse_expression(token_t *tokens,int token_count) {
-    expr_t root;
-    root.type = BINARY_OP;
-    root.op = ADD;  
-    
-    expr_t *left = malloc(sizeof(expr_t));
-    left->type = LITERAL;
-    char *left_value = malloc(sizeof(*(tokens[0].value)));
-    strcpy(left_value, tokens[0].value);
-    left->value = left_value;
-    
-    expr_t *right = malloc(sizeof(expr_t));
-    right->type = LITERAL;
-    char *right_value = malloc(sizeof(*(tokens[2].value)));
-    strcpy(right_value, tokens[2].value);
-    right->value = right_value;
+expr_t *turn_token_into_expr(token_t token) {
+    expr_t *expr = malloc(sizeof(expr_t));
+    if (token.type == INTEGER) {
+        expr->type = LITERAL;
+        char *value = malloc(sizeof(*token.value));
+        strcpy(value, token.value);
+        expr->value = value;
+    } else {
+        expr->type = BINARY_OP;
+        if (token.type == ADD_OPERATOR) expr->op = ADD;
+        else if (token.type == SUBTRACT_OPERATOR) expr->op = SUBTRACT;
+        else if (token.type == MULTIPLY_OPERATOR) expr->op = MULTIPLY;
+    }
+    return expr;
+}
 
-    root.left_operand = left;
-    root.right_operand = right;
+expr_t *parse_expression(token_t *tokens,int token_count) {
+    // stack
+    expr_t *root = turn_token_into_expr(tokens[1]);
+    expr_t *left = turn_token_into_expr(tokens[0]);
+    expr_t *right = turn_token_into_expr(tokens[2]);
+    
+    root->left_operand = left;
+    root->right_operand = right;
 
     return root;
 }
