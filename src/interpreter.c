ed8481f2717fc1c95c6845bfd93520fec8e557e9
HyunjoonCho <jhj09016@gmail.com>
2024-01-25 00:30:52 +0900

    feat: pass integer addition case  * implement func returning int / float based on input  * properly handle size of char *

--- src/interpreter.c
+++ src/interpreter.c
@@ -1,5 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "interpreter.h"
 
+int evaluate_expr(expr_t *expr) {
+    if (expr->type == LITERAL) {
+        return atoi(expr->value);
+    }
+    return 0;
+}
+
 char *interpret_expr(expr_t *root) {
-    return "";
+    char *result = malloc(2);
+
+    if (root->type == BINARY_OP) {
+        if (root->op == ADD) {
+            int value = evaluate_expr(root->left_operand) + evaluate_expr(root->right_operand);
+            snprintf(result, sizeof(result), "%d", value);
+        } else if (root->op == SUBTRACT) {
+
+        } else if (root->op == MULTIPLY) {
+
+        }
+    }
+    return result;
 }
