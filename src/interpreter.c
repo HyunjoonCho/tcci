b01cfd2cc0622859d0d08c2c9708769b62688de8
HyunjoonCho <jhj09016@gmail.com>
2024-01-25 22:12:14 +0900

    feat: implement literal union to evaluate int/float properly ; interpreter returning char * seems improper  * especially for float, it is impossible to calculate exact digit count - refer to java BigDecimal

--- src/interpreter.c
+++ src/interpreter.c
@@ -1,26 +1,77 @@
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include "interpreter.h"
 
-int evaluate_expr(expr_t *expr) {
+literal_t *evaluate_expr(expr_t *expr) {
     if (expr->type == LITERAL) {
-        return atoi(expr->value);
-    }
-    return 0;
+        literal_t *literal = malloc(sizeof(literal_t));
+        if (strchr(expr->value, '.')) {
+            literal->is_int = false;
+            literal->value.float_value = atof(expr->value);
+            printf("FLOAT %f\n", literal->value.float_value);
+        } else {
+            literal->is_int = true;
+            literal->value.int_value = atoi(expr->value);
+            printf("INT %d\n", literal->value.int_value);
+        }
+        return literal;
+    } else if (expr->type == BINARY_OP) {
+        literal_t *l = evaluate_expr(expr->left_operand);
+        literal_t *r = evaluate_expr(expr->right_operand);
+        if (expr->op == ADD) {
+            if (l->is_int && r->is_int) {
+                l->value.int_value += r->value.int_value;
+            } else {
+                if (l->is_int) l->value.float_value = (float) l->value.int_value;
+                if (r->is_int) r->value.float_value = (float) r->value.int_value;
+                l->value.float_value += r->value.float_value;
+                l->is_int = false;
+            }
+        } else if (expr->op == SUBTRACT) {
+            if (l->is_int && r->is_int) {
+                l->value.int_value -= r->value.int_value;
+            } else {
+                if (l->is_int) l->value.float_value = (float) l->value.int_value;
+                if (r->is_int) r->value.float_value = (float) r->value.int_value;
+                l->value.float_value -= r->value.float_value;
+                l->is_int = false;
+            }
+        } else if (expr->op == MULTIPLY) {
+            if (l->is_int && r->is_int) {
+                l->value.int_value *= r->value.int_value;
+            } else {
+                if (l->is_int) l->value.float_value = (float) l->value.int_value;
+                if (r->is_int) r->value.float_value = (float) r->value.int_value;
+                l->value.float_value *= r->value.float_value;
+                l->is_int = false;
+            }
+        }
+        free(r);
+        return l;
+    } 
+
+    literal_t *zero_literal = malloc(sizeof(literal_t));
+    zero_literal->is_int = true;
+    zero_literal->value.int_value = 0;
+    return zero_literal;
 }
 
 char *interpret_expr(expr_t *root) {
-    char *result = malloc(2);
+    literal_t *result = evaluate_expr(root);
 
-    if (root->type == BINARY_OP) {
-        if (root->op == ADD) {
-            int value = evaluate_expr(root->left_operand) + evaluate_expr(root->right_operand);
-            snprintf(result, sizeof(result), "%d", value);
-        } else if (root->op == SUBTRACT) {
-
-        } else if (root->op == MULTIPLY) {
-
-        }
+    char *str;
+    if (result->is_int) {
+        printf("RESULT INT %d\n", result->value.int_value);
+        int size = snprintf(NULL, 0, "%d", result->value.int_value);
+        str = (char *)malloc(size + 1);
+        snprintf(str, size + 1, "%d", result->value.int_value);
+    } else {
+        printf("RESULT float %f\n", result->value.float_value);
+        int size = snprintf(NULL, 0, "%f", result->value.float_value);
+        str = (char *)malloc(size + 1);
+        snprintf(str, size + 1, "%f", result->value.float_value);
     }
-    return result;
+
+    return str;
 }
--- src/interpreter.h
+++ src/interpreter.h
@@ -1,7 +1,22 @@
 #ifndef INTERPRETER_H
 #define INTERPRETER_H
 
+#include <stdbool.h>
 #include "commons.h"
+
+union literal_value {
+    int int_value;
+    float float_value;
+};
+
+typedef union literal_value literal_value;
+
+struct literal_t {
+    bool is_int;
+    literal_value value;
+};
+
+typedef struct literal_t literal_t;
 
 char *interpret_expr(expr_t *root);
 
