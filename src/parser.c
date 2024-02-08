4723fac60d8d8f55bab0e033de370e98794059aa
HyunjoonCho <jhj09016@gmail.com>
2024-02-09 00:56:49 +0900

    feat: pass basic assign tests

--- src/parser.c
+++ src/parser.c
@@ -3,9 +3,15 @@
 #include <string.h>
 #include "commons.h"
 
-node_t *turn_token_into_node(token_t *token, int *paren_openings) {
+node_t *turn_token_into_node(token_t *token, int *paren_openings, type_t *id_type) {
+    if (token->type == SEMICOLON) return NULL;
     if (token->type == OPEN_PAREN || token->type == CLOSE_PAREN) {
         *paren_openings += (token->type == OPEN_PAREN) - (token->type == CLOSE_PAREN);
+        return NULL;
+    }
+
+    if (token->type == INTEGER_TYPE || token->type == FLOAT_TYPE) {
+        *id_type = token->type;
         return NULL;
     }
 
@@ -18,6 +24,15 @@ node_t *turn_token_into_node(token_t *token, int *paren_openings) {
         node->type = CONSTANT;
         node->subtype = FLOAT_CONST;
         node->value.float_value = atof(token->value);      
+    } else if (token->type == IDENTIFIER) {
+        node->type = ID;
+        node->subtype = *id_type;
+        *id_type = NULLTOKEN;
+        node->value.id_name = strdup(token->value);
+    } else if (token->type == EQ_ASSIGN) {
+        node->type = ASSIGN_OP;
+        node->subtype = EQ_ASSIGN;
+        node->op_priority = 3;
     } else {
         int paren_priority = *paren_openings;
         node->type = BINARY_OP;
@@ -41,7 +56,8 @@ node_t *assemble_tree(node_t **nodes, int start_index, int end_index) {
     node_t *current_node;
     for (int i = start_index; i < end_index; i++) {
         current_node = nodes[i];
-        if (current_node->type == BINARY_OP && current_node->op_priority >= max_priority) {
+        if ((current_node->type == BINARY_OP || current_node->type == ASSIGN_OP) && 
+             current_node->op_priority >= max_priority) {
             max_priority = current_node->op_priority;
             least_prioritized_index = i;
         }
@@ -56,13 +72,14 @@ node_t *assemble_tree(node_t **nodes, int start_index, int end_index) {
 
 node_t *parse(token_t **tokens, int token_count) {
     int parentheses_openings = 0;
-    if (token_count == 1) return turn_token_into_node(tokens[0], &parentheses_openings);
+    type_t id_type = NULLTOKEN;
+    if (token_count == 1) return turn_token_into_node(tokens[0], &parentheses_openings, &id_type);
 
     node_t **nodes = malloc(token_count * sizeof(node_t *));
 
     int node_count = 0;
     for (int i = 0; i < token_count; i++) {
-        node_t *node = turn_token_into_node(tokens[i], &parentheses_openings);
+        node_t *node = turn_token_into_node(tokens[i], &parentheses_openings, &id_type);
         if (node != NULL) nodes[node_count++] = node;
     }
 
