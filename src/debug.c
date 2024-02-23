c69a15a7d5badd66a9bf6fc184afd9951400362c
HyunjoonCho <jhj09016@gmail.com>
2024-02-23 23:34:24 +0900

    refactor: move DECL node generations step for cohesion

--- src/debug.c
+++ src/debug.c
@@ -17,9 +17,11 @@ int main() {
     token_t *tokens[] = {
         generate_test_token(INTEGER_TYPE, "int"),
         generate_test_token(IDENTIFIER, "x"),
+        generate_test_token(EQ_ASSIGN, "="),
+        generate_test_token(INTEGER_CONST, "3"),
         generate_test_token(SEMICOLON, ";"),
     };
 
-    node_t *root = parse(tokens, 3);    
+    node_t *root = parse(tokens, 5);    
     return 0;
 }
--- src/parser.c
+++ src/parser.c
@@ -43,6 +43,14 @@ node_t *turn_token_into_node(token_t *token, int *paren_openings) {
     return node;
 }
 
+node_t *generate_decl_token() {
+    node_t *decl_node = malloc(sizeof(node_t));
+    decl_node->type = DECL;
+    decl_node->subtype = DECLARATION;
+    decl_node->op_priority = 4;
+    return decl_node;
+}
+
 node_t *assemble_tree(node_t **nodes, int start_index, int end_index) {
     if (start_index == end_index - 1) return nodes[start_index];
 
@@ -52,19 +60,7 @@ node_t *assemble_tree(node_t **nodes, int start_index, int end_index) {
     node_t *current_node;
     for (int i = start_index; i < end_index; i++) {
         current_node = nodes[i];
-        if (current_node->type == TYPE_SPECIFIER) {
-            nodes[i + 1]->type = DECLARATOR; // Assume: next token is identifier
-
-            node_t *decl_node = malloc(sizeof(node_t));
-            decl_node->type = DECL;
-            decl_node->subtype = DECLARATION;
-            decl_node->left_child = current_node;
-
-            nodes[i] = decl_node;
-            least_prioritized_index = i;
-            break;
-        }
-        if ((current_node->type == BINARY_OP || current_node->type == ASSIGN_OP) && 
+        if ((current_node->type == DECL || current_node->type == BINARY_OP || current_node->type == ASSIGN_OP) && 
              current_node->op_priority >= max_priority) {
             max_priority = current_node->op_priority;
             least_prioritized_index = i;
@@ -72,9 +68,8 @@ node_t *assemble_tree(node_t **nodes, int start_index, int end_index) {
     }
 
     current_node = nodes[least_prioritized_index];
-    if (current_node->type != DECL) {
-        current_node->left_child = assemble_tree(nodes, start_index, least_prioritized_index);
-    }
+    if (current_node->type == DECL) nodes[least_prioritized_index + 1]->type = DECLARATOR; // Assume: following token must be identifier
+    current_node->left_child = assemble_tree(nodes, start_index, least_prioritized_index);
     current_node->right_child = assemble_tree(nodes, least_prioritized_index + 1, end_index);
 
     return current_node;
@@ -84,12 +79,14 @@ node_t *parse(token_t **tokens, int token_count) {
     int parentheses_openings = 0;
     if (token_count == 1) return turn_token_into_node(tokens[0], &parentheses_openings);
 
-    node_t **nodes = malloc(token_count * sizeof(node_t *));
+    node_t **nodes = malloc((token_count + 1) * sizeof(node_t *)); // Leave room for DECL node to be added
 
     int node_count = 0;
     for (int i = 0; i < token_count; i++) {
         node_t *node = turn_token_into_node(tokens[i], &parentheses_openings);
-        if (node != NULL) nodes[node_count++] = node;
+        if (node == NULL) continue;
+        nodes[node_count++] = node;
+        if (node->type == TYPE_SPECIFIER) nodes[node_count++] = generate_decl_token();
     }
 
     node_t *root = assemble_tree(nodes, 0, node_count);    
