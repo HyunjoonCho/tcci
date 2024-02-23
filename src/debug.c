bfa342232588da889ba6214a1bd20a8c5f0ea1a7
HyunjoonCho <jhj09016@gmail.com>
2024-02-23 23:06:51 +0900

    feat: pass redesigned parser simple declaration tests

--- src/debug.c
+++ src/debug.c
@@ -1,11 +1,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
 #include "commons.h"
 #include "interpreter.h"
 
+token_t *generate_test_token(type_t type, const char *value) {
+    token_t *new_token = (token_t *)malloc(sizeof(token_t));
+    new_token->type = type;
+    new_token->value = strdup(value);
+
+    return new_token;
+}
+
 int main() {
-    lexer_handle lexer = lexer_init("123");
-    token_t *token = get_next_token(lexer);
+    token_t *tokens[] = {
+        generate_test_token(INTEGER_TYPE, "int"),
+        generate_test_token(IDENTIFIER, "x"),
+        generate_test_token(SEMICOLON, ";"),
+    };
+
+    node_t *root = parse(tokens, 3);    
     return 0;
 }
--- src/parser.c
+++ src/parser.c
@@ -3,20 +3,18 @@
 #include <string.h>
 #include "commons.h"
 
-node_t *turn_token_into_node(token_t *token, int *paren_openings, type_t *id_type) {
+node_t *turn_token_into_node(token_t *token, int *paren_openings) {
     if (token->type == SEMICOLON) return NULL;
     if (token->type == OPEN_PAREN || token->type == CLOSE_PAREN) {
         *paren_openings += (token->type == OPEN_PAREN) - (token->type == CLOSE_PAREN);
         return NULL;
     }
 
-    if (token->type == INTEGER_TYPE || token->type == FLOAT_TYPE) {
-        *id_type = token->type;
-        return NULL;
-    }
-
     node_t *node = malloc(sizeof(node_t));
-    if (token->type == INTEGER_CONST) {
+    if (token->type == INTEGER_TYPE || token->type == FLOAT_TYPE) {
+        node->type = TYPE_SPECIFIER;
+        node->subtype = token->type;
+    } else if (token->type == INTEGER_CONST) {
         node->type = CONSTANT;
         node->subtype = INTEGER_CONST;
         node->value.int_value = atoi(token->value);
@@ -26,8 +24,6 @@ node_t *turn_token_into_node(token_t *token, int *paren_openings, type_t *id_typ
         node->value.float_value = atof(token->value);      
     } else if (token->type == IDENTIFIER) {
         node->type = ID;
-        node->subtype = *id_type;
-        *id_type = NULLTOKEN;
         node->value.id_name = strdup(token->value);
     } else if (token->type == EQ_ASSIGN) {
         node->type = ASSIGN_OP;
@@ -56,6 +52,18 @@ node_t *assemble_tree(node_t **nodes, int start_index, int end_index) {
     node_t *current_node;
     for (int i = start_index; i < end_index; i++) {
         current_node = nodes[i];
+        if (current_node->type == TYPE_SPECIFIER) {
+            nodes[i + 1]->type = DECLARATOR; // Assume: next token is identifier
+
+            node_t *decl_node = malloc(sizeof(node_t));
+            decl_node->type = DECL;
+            decl_node->subtype = DECLARATION;
+            decl_node->left_child = current_node;
+
+            nodes[i] = decl_node;
+            least_prioritized_index = i;
+            break;
+        }
         if ((current_node->type == BINARY_OP || current_node->type == ASSIGN_OP) && 
              current_node->op_priority >= max_priority) {
             max_priority = current_node->op_priority;
@@ -64,7 +72,9 @@ node_t *assemble_tree(node_t **nodes, int start_index, int end_index) {
     }
 
     current_node = nodes[least_prioritized_index];
-    current_node->left_child = assemble_tree(nodes, start_index, least_prioritized_index);
+    if (current_node->type != DECL) {
+        current_node->left_child = assemble_tree(nodes, start_index, least_prioritized_index);
+    }
     current_node->right_child = assemble_tree(nodes, least_prioritized_index + 1, end_index);
 
     return current_node;
@@ -72,14 +82,13 @@ node_t *assemble_tree(node_t **nodes, int start_index, int end_index) {
 
 node_t *parse(token_t **tokens, int token_count) {
     int parentheses_openings = 0;
-    type_t id_type = NULLTOKEN;
-    if (token_count == 1) return turn_token_into_node(tokens[0], &parentheses_openings, &id_type);
+    if (token_count == 1) return turn_token_into_node(tokens[0], &parentheses_openings);
 
     node_t **nodes = malloc(token_count * sizeof(node_t *));
 
     int node_count = 0;
     for (int i = 0; i < token_count; i++) {
-        node_t *node = turn_token_into_node(tokens[i], &parentheses_openings, &id_type);
+        node_t *node = turn_token_into_node(tokens[i], &parentheses_openings);
         if (node != NULL) nodes[node_count++] = node;
     }
 
